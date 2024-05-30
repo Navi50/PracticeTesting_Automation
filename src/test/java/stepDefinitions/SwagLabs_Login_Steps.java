fa1134b7a7c90dc38ad24cfdfaef878cf70e4671
Navi <navitony5983@gmail.com>
2024-05-30 08:58:42 +0530

    Commiting the changes 30-05-2024

---
+++ src/test/java/stepDefinitions/SwagLabs_Login_Steps.java
@@ -0,0 +1,23 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class SwagLabs_Login_Steps {
+    @When("User enter the Username")
+    public void user_enter_the_username() {
+        System.out.println("User enter the Username");
+    }
+    @When("User enter the Pasword")
+    public void user_enter_the_pasword() {
+        System.out.println("User enter the Username");
+    }
+    @When("User click on the Login")
+    public void user_click_on_the_login() {
+        System.out.println("User enter the Username");
+    }
+    @Then("Swaglabs Homepage should be loaded")
+    public void swaglabs_homepage_should_be_loaded() {
+        System.out.println("User enter the Username");
+    }
+
+}
