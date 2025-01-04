1a24956fb07b04f58722aa63a67294a32cf33b3f
Navi <navitony5983@gmail.com>
2025-01-04 09:58:53 +0530

    Commiting the changes 04-01-2025 Updated extent reports

---
+++ src/main/java/utils/ExtentReportsUtil.java
@@ -0,0 +1,110 @@
+package utils;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+
+import io.cucumber.plugin.ConcurrentEventListener;
+import io.cucumber.plugin.event.*;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class ExtentReportsUtil implements ConcurrentEventListener {
+
+
+    public static ExtentReports extent = new ExtentReports();
+    public static ThreadLocal<ExtentTest> scenarioTest = new ThreadLocal<>();
+    public static String testResultPath;
+    public static String application = "SWAGLABS";
+
+    public void onTestRunStarted(TestRunStarted event){
+        testResultPath = "D:\\Learning\\Reports\\";
+        LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy-HH-mm-ss");
+        String timestamp = dtf.format(localDateTime);
+        String reportPath = testResultPath+application+timestamp+".html";
+
+        ExtentSparkReporter spark = new ExtentSparkReporter(reportPath);
+        spark.config().setReportName("Automation Report");
+        spark.config().setDocumentTitle("Automation Report");
+        spark.config().setTheme(Theme.STANDARD);
+
+        extent.attachReporter(spark);
+        extent.setSystemInfo("Application",application);
+        extent.setSystemInfo("OS","Windows");
+    }
+
+    public void onTestRunFinished(TestRunFinished event){
+        extent.flush();
+    }
+
+    public static void addLog(String log){
+        extent.addTestRunnerOutput(log);
+    }
+
+    public void onTestCaseStarted(TestCaseStarted event){
+        ExtentTest test = extent.createTest(event.getTestCase().getName());
+        scenarioTest.set(test);
+
+    }
+
+
+    public void onTestStepFinished(TestStepFinished event){
+        String step = event.getTestStep().getCodeLocation().substring(16);
+        String screenShotPath = takeScreenshot(step);
+        try {
+            if (event.getResult().getStatus().is(Status.FAILED)) {
+
+                scenarioTest.get().createNode(step).addScreenCaptureFromPath(screenShotPath).pass("Step Failed :"+step);
+
+            } else {
+                scenarioTest.get().createNode(step).addScreenCaptureFromPath(screenShotPath).pass("Step Passed: "+step);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static String takeScreenshot(String path){
+
+        if (DriverManager.getDriver()==null){
+            return null;
+        }
+
+        File srcFile = ((TakesScreenshot)DriverManager.getDriver()).getScreenshotAs(OutputType.FILE);
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("ddMMyyyyHHmmss"));
+        String screenshotpath = "D:\\Learning\\Reports\\"+path+"_"+time+".png";
+
+        try{
+            FileUtils.copyFile(srcFile, new File(screenshotpath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return screenshotpath;
+    }
+
+    public void onTestCaseFinished(TestCaseFinished event){
+        if (event.getResult().getStatus().is(Status.PASSED)){
+            scenarioTest.get().pass(Status.PASSED.toString());
+        }else{
+            scenarioTest.get().fail(Status.FAILED.toString());
+        }
+    }
+
+    @Override
+    public void setEventPublisher(EventPublisher eventPublisher) {
+        eventPublisher.registerHandlerFor(TestRunStarted.class, this::onTestRunStarted);
+        eventPublisher.registerHandlerFor(TestCaseStarted.class, this::onTestCaseStarted);
+        eventPublisher.registerHandlerFor(TestCaseFinished.class, this::onTestCaseFinished);
+        eventPublisher.registerHandlerFor(TestStepFinished.class, this::onTestStepFinished);
+        eventPublisher.registerHandlerFor(TestRunFinished.class, this::onTestRunFinished);
+
+    }
+}
