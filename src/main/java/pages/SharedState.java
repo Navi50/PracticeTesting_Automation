08ec6e02f4f40403bcfa2566a73dc8a49c79a55d
Navi <navitony5983@gmail.com>
2025-01-06 22:08:20 +0530

    Commiting the changes 06-01-2025

---
+++ src/main/java/pages/SharedState.java
@@ -0,0 +1,14 @@
+package pages;
+
+public class SharedState {
+    private String sharedData;
+
+    public String getSharedData(){
+        return sharedData;
+    }
+
+    public void setSharedData(String sharedData){
+        this.sharedData = sharedData;
+    }
+
+}
